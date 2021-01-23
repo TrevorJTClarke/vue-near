a9795e2365b287bdd355c7ae953b05e650998955
Trevor Clarke <trevorjtclarke@gmail.com>
2021-01-23 00:09:53 -0800

    Initial release of vue-near

--- README.md
+++ README.md
@@ -1,2 +1,151 @@
-# vue-near
-A vue 3 plugin for NEAR API. Useful for building dapps with NEAR blockchain quickly!
+<br />
+<br />
+
+<p>
+<img src="https://near.org/wp-content/themes/near-19/assets/img/neue/logo.svg?t=1600963474" width="200">
+</p>
+
+<br />
+<br />
+
+# Vue Near
+A vue 3 plugin for NEAR API. Useful for building dapps with NEAR blockchain quickly! This plugin combines the simplicity of [near-api-js](https://github.com/near/near-api-js/) to the convenience of vuejs methods available to all components.
+
+## Installation
+
+### 1. Install Plugin
+```bash
+# yarn
+yarn add @tjtc/vue-near
+
+# npm
+npm install @tjtc/vue-near
+```
+
+### 2. Add CDN NEAR-API-JS
+
+Add this script tag to your `index.html` file.
+```
+<script src="https://cdn.jsdelivr.net/gh/nearprotocol/near-api-js/dist/near-api-js.js" ></script>
+```
+
+
+### 3. Import
+
+```js
+// In main.js
+import { createApp } from 'vue'
+import VueNear from "@tjtc/vue-near"
+
+const app = createApp(App)
+
+app.use(VueNear, {
+  // Needs the environment for the correct RPC to use
+  env: process.env.NODE_ENV || 'development',
+  config: {
+    appTite: 'Cool dApp',
+    contractName: 'cool_dapp.testnet',
+  },
+})
+
+app.mount('#app')
+```
+
+## Usage
+
+```js
+// this.$near -- has all the bootstrapped near-api-js methods
+// in addition to some quick helpers
+// ALL methods available within any component
+console.log(this.$near.user.accountId)
+// -> 'cool_user.testnet'
+
+// Sign in a user, via web wallet
+this.$near.loginAccount()
+
+// logout a user
+this.$near.logoutAccount()
+
+// get a contract with executable methods
+this.$near.getContractInstance('cool_contract.testnet', {
+  changeMethods: ['set_something'],
+  viewMethods: ['get_something'],
+})
+```
+
+## API methods
+
+### `$near`
+
+This is the base global instance of near-api-js, upon application start this instance utilizes a fully configured connection, allowing immediate use within all components.
+For more information on what is available (not documented below) see [full docs here](https://near.github.io/near-api-js/). Specific methods [available here](https://near.github.io/near-api-js/classes/_near_.near.html)
+
+```js
+await this.$near.sendTokens(10, 'from_account.testnet', 'to_account.testnet')
+```
+
+### `loginAccount()`
+
+Sign in a user, via web wallet. Will redirect user to the near web wallet configured based on environment. Upon success, user will land back on this application with permissions. This will continue to use any user data that is already logged in (see localStorage) if available.
+
+```js
+this.$near.loginAccount()
+```
+
+### `logoutAccount()`
+
+Fully removes user data in localStorage as well as within all $near instances.
+
+```js
+this.$near.logoutAccount()
+```
+
+### `getContractInstance()`
+
+Load a contract with all available methods for easy interaction with the blockchain deployed contract. Configure which account the contract is available at, and provide method names for each type, then contract methods can be executed with returned instance.
+
+```js
+const contract = this.$near.getContractInstance('cool_contract.testnet', {
+  changeMethods: ['set_something'],
+  viewMethods: ['get_something'],
+})
+
+// call method
+await contract.set_something()
+
+// view method
+await contrat.get_something()
+```
+
+### `loadAccount()`
+
+Load the user account into $near instance, making all helper methods and details available.
+
+```js
+this.$near.loadAccount()
+
+// Now user info and methods are available:
+this.$near.user
+
+// Where you can access things like:
+this.near.user.accountId
+this.near.user.balance
+```
+
+### `$nearInit`
+
+In rare cases, you may want to fully re-instantiate the base $near instance. Using this method will fully clear any/all data configured within $near and re-bind a new near-api-js class, configured with built env variables.
+
+## Tests
+
+I would love help writing tests. ❤️
+
+## License
+
+[MIT](LICENSE.txt) License
+
+----
+
+### Refill My ☕️?
+
+If you feel this helped you in some way, you can tip `tjtc.near`
---
+++ src/index.js
@@ -0,0 +1,161 @@
+// Optional config, can be passed in before plugin install:
+// {
+//   appTitle: '',
+//   contractName: '',
+// }
+export function getConfig(env, options = {}) {
+  const config = {
+    ...options,
+    appTitle: options.appTitle || 'NEAR',
+    contractName: options.contractName || 'test.near',
+  }
+
+  switch (env) {
+    case 'production':
+    case 'mainnet':
+      return {
+        ...config,
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        explorerUrl: 'https://explorer.near.org',
+        walletUrl: 'https://wallet.near.org',
+        helperUrl: 'https://helper.mainnet.near.org'
+      }
+    case 'development':
+    case 'testnet':
+      return {
+        ...config,
+        networkId: 'default',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        explorerUrl: 'https://explorer.testnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org'
+      }
+    case 'betanet':
+      return {
+        ...config,
+        networkId: 'betanet',
+        nodeUrl: 'https://rpc.betanet.near.org',
+        explorerUrl: 'https://explorer.betanet.near.org',
+        walletUrl: 'https://wallet.betanet.near.org',
+        helperUrl: 'https://helper.betanet.near.org'
+      }
+    case 'local':
+      return {
+        ...config,
+        networkId: 'local',
+        nodeUrl: 'http://localhost:3030',
+        keyPath: `${process.env.HOME}/.near/validator_key.json`,
+        walletUrl: 'http://localhost:4000/wallet',
+      }
+    case 'test':
+    case 'ci':
+      return {
+        ...config,
+        networkId: 'shared-test',
+        nodeUrl: 'https://rpc.ci-testnet.near.org',
+        masterAccount: 'test.near'
+      }
+    case 'ci-betanet':
+      return {
+        ...config,
+        networkId: 'shared-test-staging',
+        nodeUrl: 'https://rpc.ci-betanet.near.org',
+        masterAccount: 'test.near'
+      }
+    default:
+      throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`)
+  }
+}
+
+export class VueNear {
+  constructor(env, config) {
+    // loading via CDN, requires adding this line to index.html:
+    // <script src="https://cdn.jsdelivr.net/gh/nearprotocol/near-api-js/dist/near-api-js.js" ></script>
+    if (!window || !window.nearApi) return
+    this.nearApi = { ...window.nearApi }
+    this.config = getConfig(env, config)
+    this.near = null
+    this.keystore = null
+    this.user = null
+
+    return this
+  }
+  
+  async loadNearProvider() {
+    this.keystore = new this.nearApi.keyStores.BrowserLocalStorageKeyStore(window.localStorage, 'nearlib:keystore:')
+    this.near = await this.nearApi.connect(Object.assign({ deps: { keyStore: this.keystore } }, this.config))
+    return this
+  }
+
+  async loadAccount() {
+    // Needed to access wallet
+    this.walletConnection = new this.nearApi.WalletConnection(this.near)
+    this.user = new this.nearApi.WalletAccount(this.near)
+
+    if (this.walletConnection.getAccountId()) {
+      this.user.accountId = this.walletConnection.getAccountId()
+      this.user.balance = (await this.walletConnection.account().state()).amount
+    }
+
+    return this
+  }
+
+  async loginAccount() {
+    if (this.user && this.user.isSignedIn()) return this.user
+    const appTitle = this.config.appTitle || 'NEAR'
+    await this.user.requestSignIn(this.config.contractName, appTitle)
+
+    // returns full access key?! CAREFUL!
+    // await this.user.requestSignIn('')
+
+    // re-load account
+    return this.loadAccount()
+  }
+
+  async logoutAccount() {
+    this.walletConnection = new this.nearApi.WalletConnection(this.near)
+    this.user = new this.nearApi.WalletAccount(this.near)
+    await this.user.signOut()
+
+    this.keystore = null
+    this.user = null
+  }
+
+  async getContractInstance(contract_id, abiMethods) {
+    if (!this.user || !this.user.accountId) return
+    const account = this.walletConnection.account()
+    const abi = {
+      changeMethods: [],
+      viewMethods: [],
+      ...abiMethods,
+    }
+
+    // Sender is the account ID to initialize transactions.
+    return new this.nearApi.Contract(
+      account,
+      contract_id,
+      { ...abi, sender: account.accountId }
+    )
+  }
+
+}
+
+// Register NEAR plugin with Vue
+export default {
+  install: async (app, { env, config }) => {
+    // Add global context and methods for NEAR
+    app.config.globalProperties.$near = await new VueNear(env, config)
+    await app.config.globalProperties.$near.loadNearProvider()
+    await app.config.globalProperties.$near.loadAccount()
+
+    app.provide('$user', app.config.globalProperties.$near.user)
+
+    // enable re-instantiation
+    app.config.globalProperties.$nearInit = () => {
+      return new VueNear(env, config).loadNearProvider()
+    }
+
+    // TODO: Add convenience methods for VUEX
+  },
+}
